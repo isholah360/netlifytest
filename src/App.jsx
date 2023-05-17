aef90f7dea145c903e2248e07d96dc7c715df672
isholah360 <isholah360@gmail.com>
2023-05-16 20:07:34 -0700

    init comit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        Hello netlify
+      </div>
+
+    </>
+  )
+}
+
+export default App
